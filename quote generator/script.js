024ec51afdea2488580c5d6651091084936de66e
prashant <prashantchouhan711@gmail.com>
2024-04-30 19:50:45 +0530

    toast notfi

--- quote generator/script.js
+++ quote generator/script.js
@@ -3,6 +3,7 @@ const quote = document.querySelector(".mainquote");
 const author = document.querySelector(".author");
 const next = document.getElementById("next");
 const copybtn = document.getElementById("copy");
+const toastbox = document.querySelector(".toastbox");
 
 async function getquote(url){
     const response = await fetch(url);
@@ -12,17 +13,36 @@ async function getquote(url){
     console.log(data);
 }
 
-next.addEventListener("click" , function(){getquote(api_url) });
-copybtn.addEventListener("click", function(){
-    copyquote();
-})
-
-
-
 function copyquote(){
-    var content = quote.textContent;
-    navigator.clipboard.writeText(content).then(function(){
-        alert("copied quote");}).catch(function(error){
+    var content1 = quote.textContent;
+    var content2 = author.textContent;
+    var contentmix = content1 + '\n' + content2;
+    navigator.clipboard.writeText(contentmix).catch(function(error){
             console.error('could not copy text' , error);
         });
     };
+
+next.addEventListener("click" , function(){
+    getquote(api_url)
+    messagepopup(newquote); 
+});
+copybtn.addEventListener("click", function(){
+    copyquote();
+    messagepopup(success);
+})
+
+const success = '<i class="fa-solid fa-circle-check"></i> Successfully Copied!!';
+const newquote = '<i class="fa-solid fa-circle-check"></i> Next Quote!!';
+
+
+function messagepopup(msg){
+let message = document.createElement("div");
+    message.classList.add('popup');
+    message.innerHTML = msg;
+    toastbox.appendChild(message);
+    setTimeout(function(){
+      message.remove();
+    }, 3000)
+}
+
+
